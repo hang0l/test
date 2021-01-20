bbe10d050feed65240f67b25165a9bcdebc3d0a8
hang0l <alexander192845@gmail.com>
2021-01-20 11:29:42 +0300

    Добавлена возможность просмотра фигур

---
+++ migrations/m210120_082843_add_isActive_column_to_figure_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles adding columns to table `{{%figure}}`.
+ */
+class m210120_082843_add_isActive_column_to_figure_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('{{%figure}}', 'isActive', $this->boolean());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('{{%figure}}', 'isActive');
+    }
+}
